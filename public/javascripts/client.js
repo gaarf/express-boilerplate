d0bfc65dd87d335e2722a8b33f976c1bdcedbc05
Adrien Cahen <onlineallthetime@gmail.com>
2011-04-28 09:45:04 -0700

    lipsum

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
 
-  log('hello world!', $.fn.jquery);
-
+  console.log('hello world! jQuery v'+ $.fn.jquery);
+  $('#main p').css({color:'orange'}).animate({'font-size':'333%', 'line-height':'.8'});
 
 });
