4625c73042408cb1973873510a13dc032021dd2b
Adrien Cahen <onlineallthetime@gmail.com>
2012-02-15 18:47:23 -0800

    remove everything

--- console.js
+++
@@ -1,9 +0,0 @@
-var _ = require('underscore'),
-    repl = require('repl'),
-    app = require('./app.js');
-
-var context = repl.start("app> ").context;
-context.app = app;
-context.__ = _;
-_.extend(context, app.STUFF.model)
-
--- public/javascripts/client.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-
-  console.log('hello world! jQuery v'+ $.fn.jquery);
-
-  $('#main p')
-    .css({color:'orange'})
-    .animate({'font-size':'333%', 'line-height':'.8'});
-
-});
